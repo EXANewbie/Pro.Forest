91d07591e661a9981f1ce9f69a3ebc0ad01c40d0
kang seok il <rkd628@naver.com>
2014-07-03 15:57:09 +0900

    둘을 합침

--- ForestProj/Client/Source.cpp
+++ ForestProj/Client/Source.cpp
@@ -1,13 +1,16 @@
 #include <cstdio>
 #include <WinSock2.h>
 
+#define PORT 78911
+#define SERVER_IP_ADDRESS "10.1.7.206"
+
 void main(void)
 {
-	const char *SERVER_IP_ADDRESS = "10.1.7.206";
+
 	WSADATA wsaData;
 	SOCKET s;
 	SOCKADDR_IN ServerAddr;
-	int Port = 78911;
+
 
 	// 윈속 2.2로 초기화
 
@@ -18,15 +21,39 @@ void main(void)
 	printf("%d\n", WSAGetLastError());
 
 	ServerAddr.sin_family = AF_INET;
-	ServerAddr.sin_port = htons(Port);
+	ServerAddr.sin_port = htons(PORT);
 	ServerAddr.sin_addr.s_addr = inet_addr(SERVER_IP_ADDRESS);
 
 	// 서버에 연결
 
 	connect(s, (SOCKADDR *)&ServerAddr, sizeof(ServerAddr));
 
-	// 데이터 송신 부분
 
+	printf("%d\n", WSAGetLastError());
+	// 데이터 송신 부분
+	printf("connection success");
+	char buf[1024] = "hihi LJH";
+	
+	int t = 10;
+	while (t--)
+	{
+		int len = strlen(buf);
+		send(s, (char*)&len, sizeof(int), 0); // message header transfer
+		int a=send(s, buf, len, 0);//message body transgfer
+	}
+	//end 전송.
+	int ptr;
+	strncpy_s(buf, "end", sizeof("end"));
+	ptr = strlen(buf);
+	send(s, (char *)&ptr, sizeof(int), 0);
+	send(s, buf, ptr, 0);
+
+	printf("메시지 기다림");
+	recv(s, (char *)&ptr, sizeof(int), 0);
+	recv(s, buf, ptr, 0);
+	buf[ptr] = 0;
+
+	printf("%s\n", buf);
 	printf("%d\n", WSAGetLastError());
 
 	closesocket(s);
