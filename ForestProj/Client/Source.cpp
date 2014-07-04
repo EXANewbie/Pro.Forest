aca60cb5f8379ab8ed71f239caa5a5c8949bdc02
kang seok il <rkd628@naver.com>
2014-07-04 18:58:06 +0900

    7/4 클라 마무으리

--- ForestProj/Client/Source.cpp
+++ ForestProj/Client/Source.cpp
@@ -2,15 +2,18 @@
 #include <WinSock2.h>
 #include <thread>
 
+#include "character.h"
+
 #define PORT 78911
-#define SERVER_IP_ADDRESS "10.1.7.10"
+#define SERVER_IP_ADDRESS "10.1.7.206"
 enum packetType{ CONNECT, INIT, SET_USER, MOVE_USER, DISCONN, ERASE_USER };
 
-void receiver(SOCKET& s)
+
+void receiver(SOCKET& s,Character& myCharacter)
 {
 	char buf[1024];
 	int len;
-	int type;
+	packetType type;
 	while (true)
 	{
 		int chk=recv(s, (char*)&type, sizeof(int), 0);
@@ -18,12 +21,44 @@ void receiver(SOCKET& s)
 			printf("disconnected\n");
 			break;
 		}
+		recv(s, (char*)&len, sizeof(int), 0);		
 
-		recv(s, (char*)&len, sizeof(int), 0);
-		int y = recv(s, buf, len, 0);
-		buf[y] = '\0';
+		int end = recv(s, buf, len, 0);
+		buf[end] = '\0';
 
-		printf("%s\n", buf);
+		int indx = 0;
+		int id;
+		for (int i = 0; i < 4; ++i)
+		{
+			((char*)&id)[i] = buf[indx++];
+		}
+		
+		if (type == SET_USER)
+		{
+			int x, y;
+			for (int i = 0; i < 4; ++i)
+				((char*)&x)[i] = buf[indx++];
+			
+			for (int i = 0; i < 4; ++i)
+				((char*)&y)[i] = buf[indx++];
+			
+			printf("diff id : %d  (%d,%d)",id,x,y);
+		}
+		else if (type == INIT)
+		{
+			int x, y;
+			for (int i = 0; i < 4; ++i)
+				((char*)&x)[i] = buf[indx++];
+			
+			for (int i = 0; i < 4; ++i)
+				((char*)&y)[i] = buf[indx++];
+
+			myCharacter.setID(id);
+			myCharacter.setX(x);
+			myCharacter.setY(y);
+			printf("my id : %d, (%d,%d)", id, myCharacter.getX(), myCharacter.getY());
+		}
+		
 	}
 	
 }
@@ -52,24 +87,28 @@ void main(void)
 	connect(s, (SOCKADDR *)&ServerAddr, sizeof(ServerAddr));
 	printf("connection success");
 
-	// 데이터 수신 쓰레드 동작.
-	std::thread t(receiver, s);
 
 	// 데이터 송신 부분
 	int type;
 	int len;
+	char buf[1024] ; 
 
-
+	//CONNECT 전송.
+	type = CONNECT;
+	strncpy_s(buf, "HELLO SERVER!",13);
+	len = strlen(buf);
+	send(s, (char*)&type, sizeof(int), 0);
 	send(s, (char*)&len, sizeof(int), 0);
+	send(s, buf, len, 0);
 
-	char buf[1024] = "up down left right";
 
-	while (true)
-	{
-		int len = strlen(buf);
-		send(s, (char*)&len, sizeof(int), 0); // message header transfer
-		send(s, buf, len, 0);//message body transgfer
-	}
+	//자신의 캐릭터 생성
+	Character myCharacter;
+	
+	// 데이터 수신 쓰레드 동작.
+	std::thread t(receiver, s, myCharacter);
+	
+
 
 	t.join();
 	closesocket(s);
