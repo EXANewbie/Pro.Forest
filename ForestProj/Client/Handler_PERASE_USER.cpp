1da80d7c5d6b160405255e208329caf662599dc6
Jeong-hwan Yim <gg7308@bluehole.net>
2014-08-06 11:31:12 +0900

    이름 출력 부분에서 터지는 것을 방지했음(이름을 복사해서 처리)

--- ForestProj/Client/Handler_PERASE_USER.cpp
+++ ForestProj/Client/Handler_PERASE_USER.cpp
@@ -17,14 +17,17 @@ void Handler_PERASE_USER(Character *myChar, std::string* str)
 	for (int i = 0; i<contents.data_size(); ++i)
 	{
 		auto user = contents.data(i);
+		std::string otherName;
 		Character* other;
 		int id = user.id();
 		{
 			Scoped_Wlock SW(&chars->srw);
 			other = chars->find(id);
+			otherName = other->getName();
 			chars->erase(id);
 		}
-		printf("※ 유저 %s님과 멀어져간다!\n", other->getName());
+		printf("※ 유저 %s님과 멀어져간다!\n", otherName.c_str());
+
 	}
 	contents.clear_data();
 
