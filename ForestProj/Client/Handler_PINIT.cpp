de5253c5a01807dc2bce3c326299fbe0ff8d4f20
kang seok il <rkd628@naver.com>
2014-08-06 14:00:38 +0900

    엽

--- ForestProj/Client/Handler_PINIT.cpp
+++ ForestProj/Client/Handler_PINIT.cpp
@@ -12,20 +12,27 @@ void Handler_PINIT(int* myID, Character *myChar, std::string* str)
 	SYNCHED_CHARACTER_MAP* chars = SYNCHED_CHARACTER_MAP::getInstance();
 	SYNCHED_MONSTER_MAP* mons = SYNCHED_MONSTER_MAP::getInstance();
 
+	if (myChar != nullptr)
+	{
+		delete myChar;
+	}
+
 	INIT::CONTENTS contents;
 	contents.ParseFromString(*str);
 
 	auto user = contents.data(0);
 	int id = user.id(), x = user.x(), y = user.y();
 	std::string name = user.name();
-	int lv = user.lv(), maxHp = user.maxhp(), power = user.power(), maxexp = user.maxexp();
+	int lv = user.lv(), maxHp = user.maxhp(), power = user.power(), maxExp = user.maxexp();
+	int prtExp = user.prtexp();
 	*myID = id;
 	Character* myCharacter = new Character();
 	myCharacter->setID(id);
 	myCharacter->setName(name);
 	myCharacter->setX(x);
 	myCharacter->setY(y);
-	myCharacter->setLv(lv, maxHp, power, maxexp);
+	myCharacter->setLv(lv, maxHp, power, maxExp);
+	myCharacter->setPrtExp(prtExp);
 	*myChar = *myCharacter;
 	{
 		Scoped_Wlock SW(&chars->srw);
